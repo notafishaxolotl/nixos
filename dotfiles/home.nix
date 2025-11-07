c5b896be2533c6799ae7af38084eb185be4cc75c
notafishaxolotl <116303535+notafishaxolotl@users.noreply.github.com>
2025-11-07 14:18:18 +0000

    Update home.nix

--- dotfiles/home.nix
+++ dotfiles/home.nix
@@ -13,13 +13,8 @@ in
     inputs.zen-browser.homeModules.beta
   ];
  
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
+  programs.zen-browser.enable = true;
+
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
