b4d5669b8ae9021e501ef07aa6f85e65544376df
notafishaxolotl <116303535+notafishaxolotl@users.noreply.github.com>
2025-11-13 14:38:29 +0000

    Update tmux.nix

--- nix/tmux.nix
+++ nix/tmux.nix
@@ -4,13 +4,24 @@
 
   programs.tmux = {
     enable = true;
+    shell = "${pkgs.bash}/bin/zsh";
     clock24 = true;
-    tmuxinator.enable = true;
     baseIndex = 1;
     mouse = true;
     focusEvents = false;
     newSession = true;
     escapeTime = 0;
+    plugins = with pkgs; [
+      tmuxPlugins.better-mouse-mode
+    ];
+    programs.tmate = {
+      enable = true;
+      # FIXME: This causes tmate to hang.
+      # extraConfig = config.xdg.configFile."tmux/tmux.conf".text;
+    };
+    tmuxinator = {
+      enable = true;
+    };
   };
 
 }
