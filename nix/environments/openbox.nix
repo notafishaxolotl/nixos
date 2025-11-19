9bc4ab947849ebbc6e8a48710e34ef12a2e8fe3c
notafishaxolotl <116303535+notafishaxolotl@users.noreply.github.com>
2025-11-19 09:22:59 +0000

    Update openbox.nix

--- nix/environments/openbox.nix
+++ nix/environments/openbox.nix
@@ -6,6 +6,14 @@
   services.picom = {
     enable =true;
   };
+  services.xserver.enable = true;
+services.xserver.videoDrivers = [ "amdgpu" ]; # Ensure the amdgpu driver is specified
+
+  services.xserver.deviceSection = ''
+  Option "TearFree" "true"
+'';
+# Optional: ensure configuration is exported to /etc/X11/xorg.conf
+  services.xserver.exportConfiguration = true;
 
   environment.systemPackages = with pkgs; [
     rofi
