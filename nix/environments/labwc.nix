5d29bf617ebddf6b79d235590ec5acd62a96d6ac
notafishaxolotl <116303535+notafishaxolotl@users.noreply.github.com>
2025-12-04 09:28:10 +0000

    Update labwc.nix

--- nix/environments/labwc.nix
+++ nix/environments/labwc.nix
@@ -9,4 +9,12 @@
   environment.systemPackages = with pkgs; [
 
   ];
+  
+  services.xserver.enable = true;
+  services.xserver.displayManager.startx.enable = true;
+  services.dbus.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
 }
